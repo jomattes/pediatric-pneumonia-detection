834793e1e0fc190777e3a2f180daefeb5c1422ff
Jon Mattes <jomattes@users.noreply.github.com>
2019-10-03 23:07:46 -0700

    update epoch count

--- pneumonia_detection_model.py
+++ pneumonia_detection_model.py
@@ -251,7 +251,8 @@ def precision_m(y_true, y_pred):
 #                                        save_best_only=True)
 
 batch_size = BATCH_SIZE
-epochs = EPOCHS
+# epochs = EPOCHS
+epochs = 25
 lr = LEARNING_RATE
 
 model.compile(loss='binary_crossentropy',
