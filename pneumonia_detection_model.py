6414bd81fb707a68691e5fc16a6ca57faccebd93
Jon Mattes <jomattes@users.noreply.github.com>
2019-10-03 23:45:55 -0700

    add new model save directory

--- pneumonia_detection_model.py
+++ pneumonia_detection_model.py
@@ -269,5 +269,6 @@ history = model.fit(train_data_aug, train_labels_aug,
                     class_weight={0:74, 1:26})
 
 # save model
+fname = os.path.join(MODEL_DIR, 'keras_model.ckpt')
 saver = tf.train.Saver()
-saver.save(K.get_session(), 'keras_model.ckpt')
+saver.save(K.get_session(), fname)
